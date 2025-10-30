3cc5a88164c5847481b428b1236a67f921646866
simon5100 <simon51005100@gmail.com>
2025-10-30 22:28:30 +0700

    Proxy начало

---
+++ src/module3/proxy/Main.java
@@ -0,0 +1,9 @@
+package module3.proxy;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Создать прокси и конектор черех котоые буду вызывать методы из прошлых модуле и из этого
+    }
+}
+
